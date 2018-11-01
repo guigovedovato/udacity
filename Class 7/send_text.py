2f261258e649860e1cac478f8c4f5e5daa5fe7d7
Rodrigo Vedovato <guigovedovato@gmail.com>
2018-11-01 11:32:12 +0000

    twilo

--- Class 7/send_text.py
+++ Class 7/send_text.py
@@ -1,9 +1,9 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "ACb9102aa1bbcc3ec28d6681068e25a977"
+account_sid = ""
 # Your Auth Token from twilio.com/console
-auth_token  = "e199053a4e335694a43d832487eb3b47"
+auth_token  = ""
 
 client = Client(account_sid, auth_token)
 
